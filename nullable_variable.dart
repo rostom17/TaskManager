680aee6f9a46906d51a805d8f5cc9c214864498b
rostom17 <rostom825@gmail.com>
2024-06-13 11:30:41 +0600

    by rostom

---
+++ nullable_variable.dart
@@ -0,0 +1,16 @@
+void main() {
+  int x;
+  x = 100;
+  print(x);
+  int? y;
+  print(y);
+  String? name = "ROSTOM ALI";
+  print(name);
+  print(name.length);
+  String? nm;
+  if (nm == null) {
+    print("This is a null variable");
+  } else {
+    print(nm.length);
+  }
+}
